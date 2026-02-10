b8a0fa1bd3145f54075583a06062cd9a27290fe6
marhel28 <ammarmaulahela20@gmail.com>
2026-02-11 01:43:53 +0700

    add alls

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { sitemapPlugin } from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     tailwindcss(),
     vueDevTools(),
+
+    // 🔥 SITEMAP
+    sitemapPlugin({
+      hostname: 'https://desa-sidomukti.com',
+      routes: [
+        '/',
+        '/lokasi',
+        '/profile',
+        '/berita',
+        '/berita/agenda',
+        '/transparansi',
+        '/potensi',
+        '/galeri'
+      ]
+    })
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
